c08b57565586925171f458bacae0279614f23310
Joshuatauro <joshuatauro45@gmail.com>
2021-06-07 22:32:07 +0400

    Heroku

--- Routes/posts.routes.js
+++ Routes/posts.routes.js
@@ -4,7 +4,6 @@ const db = require('../dbConnection')
 router.get('/' ,async(req, res) => {
   const offset = req.query.l || 0
 
-  res.json({message: "hello world"})
   try{
 
     const getPostsQuery = await db.query(`SELECT 
