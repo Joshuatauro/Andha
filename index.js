8922b37c07616429d82eecaa66283242a0373940
Joshuatauro <joshuatauro45@gmail.com>
2021-06-08 15:30:48 +0400

    heroku

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, "client/build/index.html")))
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 
 }
